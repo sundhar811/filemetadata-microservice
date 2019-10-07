f926228a67e41b6bf29886686a49162223d6714a
Glitch (sundhar811-filemetadata-microservice) <none>
2019-10-07 12:28:24 +0000

    file metadata extracted

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cors = require('cors');
 
 // require and use "multer"...
 const multer = require('multer');
+const upload = multer()
 
 var app = express();
 
@@ -18,6 +19,15 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  let { file: { originalname, size } } = req;
+  res.send({
+    message: 'Your file has been successfully uploaded',
+    filename: originalname,
+    size: size
+  })
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
